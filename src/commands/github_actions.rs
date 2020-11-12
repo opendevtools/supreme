f57326cb9e5b783379b38f86aac9dfa5a844a41d
Rickard Natt och Dag <nattochdag@hey.com>
2020-11-12 16:37:12 +0100

    feat(github-actions): detect rescript projects

--- src/commands/github_actions.rs
+++ src/commands/github_actions.rs
@@ -4,22 +4,28 @@ use serde_json::json;
 use std::fs;
 
 pub fn run(no_npm: bool) -> Result<()> {
+    let has_bs_config = fs::metadata("bsconfig.json").is_ok();
+
+    if has_bs_config {
+        println!("Found ReScript project");
+    }
+
     fs::create_dir_all(".github/workflows")?;
 
     template::render(
         include_str!("../templates/github_actions/release.yml"),
         ".github/workflows/release.yml",
-        Some(json!({ "no_npm": no_npm })),
+        Some(json!({ "noNpm": no_npm, "isRescript": has_bs_config })),
     )?;
     template::render(
         include_str!("../templates/github_actions/pr_check.yml"),
         ".github/workflows/pr_check.yml",
-        None,
+        Some(json!({ "isRescript": has_bs_config })),
     )?;
     template::render(
         include_str!("../templates/github_actions/.releaserc"),
         ".releaserc",
-        Some(json!({ "no_npm": no_npm })),
+        Some(json!({ "noNpm": no_npm })),
     )?;
 
     Ok(())
