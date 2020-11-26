76f317532cff10d31bf7adba508db2e6cee4ef82
believer <rickard@hey.com>
2020-11-26 12:05:51 +0100

    feat(github-actions): add config for rust projects
    Automatically identifies a Rust project if a Cargo.toml
    exists in the project root.

--- src/commands/github_actions.rs
+++ src/commands/github_actions.rs
@@ -1,29 +1,80 @@
-use crate::utils::{helpers, template};
+use crate::utils::{format, helpers, template};
+use dialoguer::{theme::ColorfulTheme, Select};
 use helpers::Result;
 use include_dir_macro::include_dir;
 use serde_json::json;
 use std::fs;
 
-pub fn run(no_npm: bool) -> Result<()> {
-    let has_bs_config = fs::metadata("bsconfig.json").is_ok();
+mod app_type {
+    use super::*;
 
-    if has_bs_config {
-        println!("Found ReScript project, adding build step");
+    pub enum T {
+        JavaScript,
+        ReScript,
+        Rust,
     }
 
-    fs::create_dir_all(".github/workflows")?;
+    fn from_selection() -> T {
+        let selections = &["ReScript", "Rust"];
+        let selection = Select::with_theme(&ColorfulTheme::default())
+            .with_prompt("Multiple languages found, select one:")
+            .default(0)
+            .items(&selections[..])
+            .interact()
+            .unwrap();
 
-    template::render_dir(
-        include_dir!("src/templates/github_actions"),
-        ".github/workflows",
-        &json!({ "noNpm": no_npm, "isRescript": has_bs_config }),
-    )?;
+        match selection {
+            0 => T::ReScript,
+            1 => T::Rust,
+            _ => panic!("Unknown selection"),
+        }
+    }
 
-    template::render_file(
-        include_str!("../templates/.releaserc"),
-        ".releaserc",
-        Some(json!({ "noNpm": no_npm })),
-    )?;
+    pub fn make() -> T {
+        let has_bs_config = fs::metadata("bsconfig.json").is_ok();
+        let has_cargo_toml = fs::metadata("Cargo.toml").is_ok();
+
+        match (has_bs_config, has_cargo_toml) {
+            (true, false) => T::ReScript,
+            (false, true) => T::Rust,
+            (true, true) => from_selection(),
+            _ => T::JavaScript,
+        }
+    }
+}
+
+pub fn run(no_npm: bool) -> Result<()> {
+    let (template_directory, releaserc) = match app_type::make() {
+        app_type::T::ReScript => {
+            format::success("Found ReScript project");
+
+            (
+                include_dir!("src/templates/github_actions/rescript"),
+                include_str!("../templates/github_actions/release_config/.releaserc.js"),
+            )
+        }
+        app_type::T::JavaScript => {
+            format::success("Found JavaScript project");
+
+            (
+                include_dir!("src/templates/github_actions/js"),
+                include_str!("../templates/github_actions/release_config/.releaserc.js"),
+            )
+        }
+        app_type::T::Rust => {
+            format::success("Found Rust project");
+
+            (
+                include_dir!("src/templates/github_actions/rust"),
+                include_str!("../templates/github_actions/release_config/.releaserc.rs"),
+            )
+        }
+    };
+
+    let data = json!({ "name": env!("CARGO_PKG_NAME"), "noNpm": no_npm });
+
+    template::render_dir(template_directory, ".github/workflows", &data)?;
+    template::render_file(releaserc, ".releaserc", Some(&data))?;
 
     Ok(())
 }
