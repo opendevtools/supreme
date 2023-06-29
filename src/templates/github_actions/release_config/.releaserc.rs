815fc39883a76aec8c2e69b37c18cac49ec7f133
Rickard Natt och Dag <rickard@nattochdag.dev>
2023-06-29 11:39:52 +0200

    feat(github-actions): update rust templates
    Fixes #179

--- src/templates/github_actions/release_config/.releaserc.rs
+++ src/templates/github_actions/release_config/.releaserc.rs
@@ -4,13 +4,31 @@
     "@semantic-release/commit-analyzer",
     "@semantic-release/release-notes-generator",
     "@semantic-release/changelog",
-    ["@semantic-release/exec", {
-      "verifyConditionsCmd": "semantic-release-rust verify-conditions",
-      "prepareCmd": "semantic-release-rust prepare ${nextRelease.version}",
-    }],
-    ["@semantic-release/git", {
-      "assets": ["Cargo.toml", "Cargo.lock", "CHANGELOG.md"]
-    }]
+    [
+      "@semantic-release/exec",
+      {
+        "verifyConditionsCmd": "semantic-release-rust verify-conditions",
+        "prepareCmd": "semantic-release-rust prepare ${nextRelease.version}"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["Cargo.toml", "Cargo.lock", "CHANGELOG.md"]
+      }
+    ],
+    [
+      "@semantic-release/github",
+      {
+        "assets": [
+          {
+            "path": "artifacts/aarch64-apple-darwin/{{name}}-aarch64-apple-darwin.tar.gz",
+            "label": "{{name}}-${nextRelease.version}-aarch64-apple-darwin.tar.gz",
+            "name": "{{name}}-${nextRelease.version}-aarch64-apple-darwin.tar.gz"
+          }
+        ]
+      }
+    ]
   ]
 }
 
