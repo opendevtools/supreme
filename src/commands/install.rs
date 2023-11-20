73ae667d0925bafcb47d3f2b20acdb07ba198428
believer <rickard@nattochdag.dev>
2023-11-20 10:00:25 +0100

    fix(install): remove passed through flags from packages

--- src/commands/install.rs
+++ src/commands/install.rs
@@ -2,6 +2,15 @@ use crate::utils::{helpers, node};
 use helpers::Result;
 
 pub fn run(packages: Vec<String>, dev: bool, sync_lockfile: bool, global: bool) -> Result<()> {
+    let packages = packages
+        .iter()
+        // Filter out any flags
+        // This can happen when passing args using `--`
+        // For example: `supreme install is-even -- -W`
+        .filter(|p| !p.starts_with("-"))
+        .map(|p| p.to_string())
+        .collect::<Vec<String>>();
+
     match (packages, dev, sync_lockfile, global) {
         (packages, _, _, true) => node::install_global(&packages),
         (packages, _, sync_lockfile, false) if packages.is_empty() => {
