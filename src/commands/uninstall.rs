e0799e77b9d047319b54e31727633d9c02be10cd
believer <rickard@nattochdag.dev>
2023-11-20 13:42:32 +0100

    fix(uninstall): filter flags from packages

--- src/commands/uninstall.rs
+++ src/commands/uninstall.rs
@@ -1,8 +1,17 @@
 use crate::utils::{helpers, node};
 use helpers::Result;
 
-pub fn run(name: Vec<String>, global: bool) -> Result<()> {
-    node::uninstall(&name, global);
+pub fn run(packages: Vec<String>, global: bool) -> Result<()> {
+    let packages = packages
+        .iter()
+        // Filter out any flags
+        // This can happen when passing args using `--`
+        // For example: `supreme install is-even -- -W`
+        .filter(|p| !p.starts_with('-'))
+        .map(|p| p.to_string())
+        .collect::<Vec<String>>();
+
+    node::uninstall(&packages, global);
 
     Ok(())
 }
